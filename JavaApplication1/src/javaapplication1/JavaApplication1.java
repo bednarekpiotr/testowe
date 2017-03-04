9341dfedaa8ee7123dc57152bfb50339cfcad333
Piotr Bednarek <195560@edu.p.lodz.pl>
2017-03-04 23:00:36 +0100

    commit

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,13 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.mavenproject1;
+package javaapplication1;
 
 /**
  *
  * @author Piotr
  */
-public class Main {
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("master");
     }
